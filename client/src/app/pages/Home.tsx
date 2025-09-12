213734849b41267e9a3970ded5c22b8b3232aeaa
A365Nobik <matvey.yarikov@gmail.com>
2025-09-12 20:04:53 +0300

    some changes

--- client/src/app/pages/Home.tsx
+++ client/src/app/pages/Home.tsx
@@ -1,10 +1,16 @@
-export default function Home(){
-  return(
-    <div>
-        <h1>Hello</h1>
-        <div className="bg-amber-900 p-10">
-
+export default function Home() {
+  return (
+    <>
+      <title>Домашняя страница</title>
+      <div className="flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center w-[50%]">
+          <h1 className="text-3xl">Mega-Bot</h1>
         </div>
-    </div>
-  )
+        <div className="flex flex-col justify-center items-center bg-[#CBDCEB] w-[50%]">
+          <h1>Тут будут примеры запросов и ответов</h1>
+          <div className="w-40 h-20 bg-gray-800 rounded-2xl"></div>
+        </div>
+      </div>
+    </>
+  );
 }
